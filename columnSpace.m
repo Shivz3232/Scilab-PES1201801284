0ee17dfcc2cb902875f425e2bf4ac65697bbb5da
Shivu D G <48614025+Shivz3232@users.noreply.github.com>
2020-03-30 20:36:36 +0530

    Update columnSpace.m

--- columnSpace.m
+++ columnSpace.m
@@ -1,20 +1,39 @@
 function colmnSpace = columnSpace(A)
+
+    function T = normalize(T)
+        for j = 1:n-1
+            if (T(j, j) == 0 && j != 3)
+                if (T(j+1, j) != 0)
+                    X = T(j, :);
+                    T(j, :) = T(j+1, :);
+                    T(j+1, :) = X;
+    
+                elseif (j != 2 && T(j+2, j) != 0)
+                    X = T(j, :);
+                    T(j, :) = T(j+2, :);
+                    T(j+2, :) = X;
+                end
+            end
+        end
+    endfunction
     
     A = input ("\nEnter the matrix A:\n");
 
     n = length(A);
 
+    T = normalize(A);
+    
     for j = 1:n-1
         for i = j+1:n
-            A(i, j:n) = A(i, j:n) - (A(i, j) / A(j, j)) * A(j, j:n);
+            T(i, j:n) = T(i, j:n) - (T(i, j) / T(j, j)) * T(j, j:n);
         end
     end
 
     colmnSpace = [];
 
     for i = 1:n
-        if A(i, i) != 0
-            colmnSpace = [colmnSpace A(:, i)];
+        if T(i, i) != 0
+            colmnSpace = [colmnSpace T(:, i)];
         end
     end
 
